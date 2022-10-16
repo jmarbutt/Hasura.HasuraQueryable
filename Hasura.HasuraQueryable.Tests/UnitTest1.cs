33145245fc4dad62305fc382d6015a0dfd951b05
Jonathan Marbutt <jonathan@marbutt.com>
2022-10-16 16:22:41 -0500

    Adding Test Project

---
+++ Hasura.HasuraQueryable.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Hasura.HasuraQueryable.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
